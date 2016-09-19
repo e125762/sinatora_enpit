db84129584f4d97434ff36ce3d54f7602c9f0019
Keito <e125762@ie.u-ryukyu.ac.jp>
2016-09-19 07:23:53 +0000

    make link app

--- hello.rb
+++ hello.rb
@@ -1,18 +1,15 @@
 # coding: utf-8
 require 'sinatra'
 require './name'
+
 get '/' do
-=begin
-<<EOS
-<html>
-<head>
-<title>enpit_heroku</title>
-</head>
-<body>
-<h1>Hello World! ryukyu</h1>
-</body>
-</html>
-EOS
-=end
 erb :index
+end
+
+get '/win' do
+erb :win
+end
+
+get '/lose' do
+erb :lose
 end
