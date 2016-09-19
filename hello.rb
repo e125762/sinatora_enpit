6b6a5de41dfd51c088624f111a0b4d62ed00634d
Keito <e125762@ie.u-ryukyu.ac.jp>
2016-09-19 05:07:52 +0000

    add html tags

--- hello.rb
+++ hello.rb
@@ -1,5 +1,18 @@
+# -*- coding: undecided -*-
 require 'sinatra'
 
 get '/' do
+<<EOS
+<html>
+<head>
+<title>
+enpit_heroku
+</title>
+</head>
+<body>
   "<h1>Hello World! ryukyu</h1>"
+テスト
+</body>
+</html>
+EOS
 end
